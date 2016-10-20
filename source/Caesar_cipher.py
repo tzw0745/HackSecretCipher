0d08a99d3a6fdc0be199c485bf6f186d778deea0
tzw0745 <tzw0745@163.com>
2016-10-20 16:38:51 +0800

    change code style

--- source/Caesar_cipher.py
+++ source/Caesar_cipher.py
@@ -1,15 +1,15 @@
-'''
+"""
 参数：接收源字符串，密钥
 参数：密钥范围为0-25
 返回：经过凯撒加密后的字符串
-'''
-def CaesarCipher(string, key):
+"""
+def CaesarCipher(plainText, key):
     # 检查密钥范围
     if key < 0 or key > 25:
         raise ValueError
 
     encryptArr = []
-    for ch in string:
+    for ch in plainText:
         # 如果不是字符和则不用加密
         if not ch.isalpha() and not ch.isdigit():
             encryptArr.append(ch)
@@ -28,13 +28,12 @@ def CaesarCipher(string, key):
         encryptArr.append(chr(i))
     return ''.join(encryptArr)
 
-
-'''
+"""
 参数：接收加密后的字符串。可选参数为key
 返回：无key时暴力破解凯撒加密，返回长26的数组。每个元素为：[key, originStr]
 返回：有key时返回用key解凯撒加密后的结果：[key, originStr]
-'''
-def DeCaesarCipher(string, key=None):
+"""
+def DeCaesarCipher(cipherText, key=None):
     if key:
         r = range(key, key+1, 1)
     else:
@@ -43,7 +42,7 @@ def DeCaesarCipher(string, key=None):
     # 如果带key则r只有一个元素，不带key时r有26个元素
     for key in r:
         decryptArr = []
-        for ch in string:
+        for ch in cipherText:
             # 如果不是字符则不用解密
             if not ch.isalpha() and not ch.isdigit():
                 decryptArr.append(ch)
@@ -67,18 +66,18 @@ def DeCaesarCipher(string, key=None):
 
 
 def main():
-    secretStr = 'My bank password is 123456'
+    plainText = 'My bank password is 123456'
     keyNum = 8
-    print('The origin string    :', secretStr)
+    print('The origin string    :', plainText)
     print('The Caesar Cipher Key:', keyNum)
 
-    encryptStr = CaesarCipher(secretStr, keyNum)
+    cipherText = CaesarCipher(plainText, keyNum)
     print('encrypting...')
-    print('After Caesar Cipher  :', encryptStr)
+    print('After Caesar Cipher  :', cipherText)
 
     print('\nforce decrypting...')
-    for key, decryptStr in DeCaesarCipher(encryptStr):
-        print('Key #{0}: {1}'.format(key, decryptStr))
+    for key, plainText in DeCaesarCipher(cipherText):
+        print('Key #{0}: {1}'.format(key, plainText))
 
 if __name__ == '__main__':
     main()
